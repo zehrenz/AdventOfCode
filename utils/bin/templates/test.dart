c8b80849c5e39197abaf822e541a69ed395e19e4
Joseph Peters <petersjl@rose-hulman.edu>
2026-09-22 12:15:54 -0400

    Change template to new style

--- utils/bin/templates/test.dart
+++ utils/bin/templates/test.dart
@@ -1,6 +1,8 @@
 import 'package:test/expect.dart';
 import 'package:test/scaffolding.dart';
 
+enum Part { ONE, TWO }
+
 import 'package:utils/dart_utils.dart';
 
 const String DAY = '{day_num}';
@@ -8,7 +10,8 @@ void main() {
   if (DAY.isEmpty) {
     throw Exception("Please set the DAY constant to the day being tested.");
   }
-  for (var (file, p1, p2) in [('A', "", "")])
+  for (var (part, file, expected) in [
+    (Part.ONE, 'A', "")])
     group("Check sample input $file passes for part", () {
       late var input;
       setUp(() {
@@ -16,12 +19,12 @@ void main() {
           Utils.readToString('../test_inputs/day$DAY-$file.txt'),
         );
       });
-      test("1", () {
-        expect(solvePart1(input), p1.toString());
-      }, skip: p1.isEmpty);
-      test("2", () {
-        expect(solvePart2(input), p2.toString());
-      }, skip: p2.isEmpty);
+      test(part == Part.ONE ? "1" : "2", () {
+        if (part == Part.ONE)
+          expect(solvePart1(input), expected.toString());
+        else
+          expect(solvePart2(input), expected.toString());
+      });
     });
 
   group("Check actual input passes for part", () {
